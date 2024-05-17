361982955803b1a373930c9c14ba539912b0845b
Jamal Hassunizadeh <113135025+jamalvh@users.noreply.github.com>
2024-05-17 02:50:49 -0400

    upd mt

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1150668484"
+    serviceWorkerVersion: "1001262714"
   }
 });
