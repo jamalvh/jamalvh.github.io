2e68d57708306fe3f35cc3b0428bf962e2724812
jamalvh <113135025+jamalvh@users.noreply.github.com>
2024-06-17 22:27:17 -0400

    mt

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6150531938a28df149d7817be219d533",
+const RESOURCES = {"flutter_bootstrap.js": "7ead2c5b2e476b85adb5d9d470409896",
 "version.json": "c8091615d288bbf7fb28601e03eb4931",
-"index.html": "217e2cbaa25aefe1d32a98435c9c3269",
-"/": "217e2cbaa25aefe1d32a98435c9c3269",
+"index.html": "95693ea76fee0037108a8a87efc3725e",
+"/": "95693ea76fee0037108a8a87efc3725e",
 "main.dart.js": "d3c75d2f3edbe9c3910916dbaf05c960",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "88347cd52d7d7851e0d5a952df99759c",
