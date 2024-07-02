9292bb86307a3687d499c9ce0a2a2811a08e889f
jamalvh <113135025+jamalvh@users.noreply.github.com>
2024-07-01 23:47:11 -0400

    Added badges, quick-type options, better user info cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2971825594"
+    serviceWorkerVersion: "1797425519"
   }
 });
