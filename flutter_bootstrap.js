dbc7cda933083f7a62997a8220dd2e76c8a4e59b
jamalvh <jamalvh@umich.edu>
2025-05-17 15:33:39 -0400

    Icons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4168777791"
+    serviceWorkerVersion: "3955063287"
   }
 });
